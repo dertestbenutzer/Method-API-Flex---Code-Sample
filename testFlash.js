e7f0d3cbec354f54cf017698e35ae95d2292773b
dertestbenutzer <der.test.benutzer@googlemail.com>
2011-04-19 06:47:40 -0700

    Edited testFlash.js via GitHub

--- testFlash.js
+++ testFlash.js
@@ -1,5 +1,5 @@
 (function () {
     /* Change APPLICATION_KEY to the one specified in your bundle.xml and
 	 * URL_TO_SWF to the URL of the swf file (without the extension!) */
-    streamwork.flash.registerModule("APPLICATION_KEY", "URL_TO_SWF");
+    streamwork.flash.registerModule("APPLICATION_KEY", "http://swgae.googlecode.com/svn/RSSViewer");
 })();
