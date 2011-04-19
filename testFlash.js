9f91daf735103fa8d0032f483d6859b11f58a6b8
dertestbenutzer <der.test.benutzer@googlemail.com>
2011-04-19 06:48:04 -0700

    Edited testFlash.js via GitHub

--- testFlash.js
+++ testFlash.js
@@ -1,5 +1,5 @@
 (function () {
     /* Change APPLICATION_KEY to the one specified in your bundle.xml and
 	 * URL_TO_SWF to the URL of the swf file (without the extension!) */
-    streamwork.flash.registerModule("APPLICATION_KEY", "http://swgae.googlecode.com/svn/RSSViewer");
+    streamwork.flash.registerModule("com.sap.mueller.ruediger.flexcodesample", "http://swgae.googlecode.com/svn/RSSViewer");
 })();
